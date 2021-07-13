aea41c558a512f5190e0f2ff8396185bbdd29c15
jackson-89 <ccc012105@coderacademy.edu.au>
2021-07-13 15:40:21 +1000

    Created a while loop for input validation to start the game

--- application.py
+++ application.py
@@ -1,16 +1,18 @@
+print("Welcome to 'Escape the Depths', a text-based survival game made with Python")
+print("In this game you will be tasked to escape a dark prison and fight for your freedom")
+
+try:
+    start_game = input("Do you dare to play? Press Y to start: ")
+    while start_game != "Y" and start_game != "y":
+        print("Please enter Y if you want to continue!")
+        start_game = input("Do you dare to continue? (Y/N): ")
+except KeyboardInterrupt:
+    print("Goodbye...For now")
+    
 
 
-def intro():
-    try:
-        user_input = ""
-        while user_input != "Y" or "y":
-            user_input = input("Press Y to continue")
-        if user_input == "Y" or "y":
-            print("Congrats you made a blank resume")
-            file = open("resume.txt", "x")  
-    except:
-        print("Please enter a valid response (Y/N")
 
-intro()
 
-        
+    
+    
+
