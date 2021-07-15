97cb76b5c444f58c62151fb8ef17b53b15ea2491
jackson-89 <ccc012105@coderacademy.edu.au>
2021-07-15 10:39:29 +1000

    Completed the feature for the first set of options

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ def intro():
     print('\a')
     print("You jolt awake and scan around the dark room you lay in")
     time.sleep(a)
-    print("The dark prison room bears nothing but a mouldy bed, sad little bench and a --- you notice something off")
+    print("The dark prison room bears nothing but a mouldy bed, a sad little bench and a --- you notice something off")
     time.sleep(a)
     print("The doors for the room has been left open! The large knock was the sound of it loosley slamming")
     time.sleep(a)
@@ -20,20 +20,34 @@ def intro():
     print("You sneak through the gate and make your way up to the hallway where you are met with three different paths")
     time.sleep(a)
     print("")
-    time.sleep(a)
-    #first_choice()
+    first_choice()
 
 def first_choice():
-    first_choice_options = ["a","b","c"]
+    list_of_paths = ["a","b","c"]
     user_choice = ""
-    while user_choice not in first_choice_options:
-        print(''' Where would you like to travel?)
+    while user_choice not in list_of_paths:
+        print(''' Here are the different paths you can take:
+        a) Towards the wardens room
+        b) Towards the prison cafeteria
+        c) Towards the guard bunkers''')
+        
+        user_choice = input("Enter your option: ")
+        
+        if user_choice == list_of_paths[0]:
+            warden_path()
+        elif user_choice == list_of_paths[1]:
+            cafeteria_path()
+        elif user_choice == list_of_paths[2]:
+            guard_path()
 
+def warden_path():
+    print("warden")
 
+def cafeteria_path():
+    print("cafeteria")
 
-
-
-
+def guard_path():
+    print("guard")
 
 
 print("Welcome to 'Escape the Depths', a text-based survival game made with Python")
